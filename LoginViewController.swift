e2aafadbe1b2eabb084f37e85f1867e47d8ede72
Wayne Shan <ishawnshan@gmail.com>
2016-01-06 11:02:20 -0500

    Show Tweets with ListTimeLine

---
+++ LoginViewController.swift
@@ -0,0 +1,122 @@
+//
+//  ViewController.swift
+//  TwitterFavricClient
+//
+//  Created by Wei Shan on 1/4/16.
+//  Copyright © 2016 Wei Shan. All rights reserved.
+//
+
+import UIKit
+import TwitterKit
+
+
+class LoginViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //let twitter = Twitter.sharedInstance()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        /*
+        let client = TWTRAPIClient()
+        let statusesShowEndpoint = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let params = ["count": "5"]
+        var clientError : NSError?
+        
+        let request = Twitter.sharedInstance().APIClient.URLRequestWithMethod("GET", URL: statusesShowEndpoint, parameters: params, error: &clientError)
+        //Get auth
+        twitter.startWithConsumerKey("cmsUx91ek6lqYW6Pg0lzG2xwk", consumerSecret: "JgZPQlLumGKVKeXi90K232nBapw%3D")
+        //let oauthSigning = TWTROAuthSigning(authConfig:twitter.authConfig, authSession:twitter.session())
+        //let authHeaders = oauthSigning.OAuthEchoHeadersToVerifyCredentials()
+        //let mutalRequest: NSMutableURLRequest =
+        
+        
+        //Send Request
+        client.sendTwitterRequest(request) { (response, data, connectionError) -> Void in
+            if (connectionError == nil) {
+//                var jsonError : NSError?
+                do{
+                    let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments)
+                    let tweets = TWTRTweet.tweetsWithJSONArray(json as! [AnyObject])
+                    let tweetView = TWTRTweetView(tweet: tweets![0] as! TWTRTweet)
+                    tweetView.center = CGPointMake(self.view.center.x, self.topLayoutGuide.length + tweetView.frame.size.height / 2);
+                    self.view.addSubview(tweetView)
+                    self.viewDidLoad()
+                } catch is NSError {
+                    print("error")
+                }
+                
+            }
+            else {
+                print("Error: \(connectionError)")
+            }
+        }
+        
+        
+        /*
+        if Twitter.sharedInstance().session() != nil {
+            
+            let userID = Twitter.sharedInstance().session()!.userID
+            let client = TWTRAPIClient(userID: userID)
+            let tweetIDs = ["20", "510908133917487104"]
+            
+            client.loadTweetsWithIDs(tweetIDs) { (tweets, error) -> Void in
+                print(tweets!.count)
+                for i in 1...tweets!.count {
+                    let unwrappedTweet = tweets![i - 1]
+                    let tweetView = TWTRTweetView(tweet: unwrappedTweet as! TWTRTweet)
+                    tweetView.center = CGPointMake(self.view.center.x, self.topLayoutGuide.length + tweetView.frame.size.height / 2 + CGFloat(i * 50));
+                    self.view.addSubview(tweetView)
+                }
+                /*
+                {
+                    NSLog("Tweet load error: %@", error!.localizedDescription);
+                }*/
+
+            }
+        }
+        */
+*/
+
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        let logInButton = TWTRLogInButton { (session, error) in
+            if let unwrappedSession = session {
+//                let alert = UIAlertController(title: "Logged In",
+//                    message: "User \(unwrappedSession.userName) has logged in",
+//                    preferredStyle: UIAlertControllerStyle.Alert
+//                )
+//                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+//                self.presentViewController(alert, animated: true, completion: nil)
+                NSLog("Login with UserID: %@", unwrappedSession.userID)
+                self.performSegueWithIdentifier("gotoTimeline", sender: self)
+            } else {
+                NSLog("Login error: %@", error!.localizedDescription);
+            }
+        }
+        
+        // TODO: Change where the log in button is positioned in your view
+        logInButton.center = self.view.center
+        self.view.addSubview(logInButton)
+    }
+
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "gotoTimeline" {
+            (segue.destinationViewController as! UserTimelineViewController).logedin = true
+        }
+    }
+
+}
+
---
+++ UserTimelineViewController.swift
@@ -0,0 +1,51 @@
+//
+//  UserTimeLineViewController.swift
+//  TwitterFavricClient
+//
+//  Created by Wei Shan on 1/5/16.
+//  Copyright © 2016 Wei Shan. All rights reserved.
+//
+
+import UIKit
+import TwitterKit
+
+class UserTimelineViewController: TWTRTimelineViewController {
+    
+    var logedin = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if !logedin {
+            Twitter.sharedInstance().logOut()
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        let session = Twitter.sharedInstance().sessionStore.session()
+        if  session == nil {
+            self.performSegueWithIdentifier("gotoLogin", sender: self)
+            print("We got here")
+        } else {
+            //Show Tweets
+            let userID = session!.userID
+            let client = TWTRAPIClient(userID: userID)
+            self.dataSource = TWTRListTimelineDataSource(listSlug: "twitter-syndication-team", listOwnerScreenName: "benward", APIClient: client)
+            self.showTweetActions = true
+
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "gotoLogin" {
+            segue.destinationViewController as! LoginViewController
+        }
+    }
+    
+    
+    
+}
